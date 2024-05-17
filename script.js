219769d140327f4fdfae5c8e497e63d3c91312b3
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-05-17 16:15:15 +0200

    updated script

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const apiKey = "gQ+zx0DqBq4Z73iNUYQJYQ==Ovxd8MIf5f13ogJE";
+const buttonEl = document.querySelector("button");
+
+const options = {
+  method : "GET",
+  headers : {
+      "X-Api-Key" : apiKey,
+  }
+}
+
+buttonEl.addEventListener("click", () => { fetch('https://api.api-ninjas.com/v1/quotes?',options)
+  .then(res => {
+  return res.json();
+})
+.then(data => {
+  
+const adviceEl = document.querySelector("h2");
+adviceEl.innerHTML = data[0].quote;
+
+})
+.catch(error => {
+  console.error('Error fetching or processing data:', error);
+});
+})
 
